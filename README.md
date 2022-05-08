d2fb95b575c17abe774b326c45db81ff5d53d72b
Artem Motalov <artemmmmmotalov@gmail.com>
2022-05-08 17:15:45 +0300

    v2.1 - html interface - description, console output, commands edit

--- README.md
+++ README.md
@@ -17,13 +17,15 @@ v1.0 (branch: 'develop_v1.0')
 - This code shows how robot uses additional software.
 - All include their own methods, seperately created.
 
-v2.0 (branch: 'develop_v2.0')
+v2.0 (branch: 'develop_v2.0' and so on...)
 - robot is created by function-constructor
 - Methods are written in constructor's prototype
 - THEREFORE: new robot creating is simplifed
 
-
-
+v2.1
+- added html interface with description
+- added console output
+- added commands adding/deleting/editing
 
 
 -------------------------------------------------------
@@ -32,13 +34,23 @@ TO ADD:
 
 + upgraded system of 
 - fire system (reload, ammo amount, )
-  a) choosing enemies
-  b) shooting
-  c) alerting('Target eliminated.')
+  - a) choosing enemies
+  - b) shooting
+  - c) alerting('Target eliminated.')
 - command adding and editing
+  + a) command adding
+  - b) command editing
 - reporting + console.log
 
 - html interface page
-- built-in console
+  + basic implementation
+  + info page
+  - usage with buttons
+  - graphical robot interpretation
+  - graphical command interpretations
 - buttons - to request info and give commands
+- built-in console
+  + output console
+  - input console (or other way of interaction, e.g. buttons)
+
 
--- robot.js
+++ robot.js
@@ -6,22 +6,59 @@ Robot.prototype = {
   // main core methods
 
   getMainInfo() {
+    console.log(`Robot ${this.name}, cpu version ${this.cpuVersion}`);
+
     return `Robot ${this.name}, cpu version ${this.cpuVersion}`;
   },
 
   getAdditionalInfo() {
-    return `Update version: ${this.softwareVersion}`;
+    console.log(`Updated version: ${this.softwareVersion}`);
+
+    return `Updated version: ${this.softwareVersion}`;
+  },
+
+  getGreeting() {
+    console.log(`Hello Master. I am ${this.name}. Waiting for your command.`);
+
+    return `Hello Master. I am ${this.name}. Waiting for your command.`;
   },
 
   updateRobot(updateVersion) {
     this.softwareVersion = updateVersion;
+    console.log(`${this.name} updated to ${updateVersion}`);
 
     return `${this.name} updated to ${updateVersion}`;
+  },
+
+  // commands menu
+
+  addCommand: function(command_name, command) {
+    Robot.prototype[command_name] = command;
+
+    console.log(`Your command ${command_name} was added. Try it!`);
+  },
+
+  deleteCommand: function(command_name) {
+    delete Robot.prototype[command_name];
+
+    console.log(`Your command ${command_name} was removed.`);
+  },
+
+  editCommand: function(command_name, command) {
+    if (command_name in Robot.prorotype) {
+      Robot.prototype[command_name] = command;
+
+      console.log(`Your command ${command_name} was edited. Try it!`);
+    } else {
+      console.log(`Command ${command_name} not found. Nothing to change.`);
+    }
   },
 
   // navigation methods
 
   getCoords: function() {
+    console.log(`x=${this.coords.x} y=${this.coords.y}`);
+
     return `x=${this.coords.x} y=${this.coords.y}`;
   },
 
@@ -30,22 +67,32 @@ Robot.prototype = {
     this.target.coords.y = targY;
   },
 
+  getTargetCoords: function() {
+    console.log(`x=${this.target.coords.x} y=${this.target.coords.y}`);
+
+    return `x=${this.target.coords.x} y=${this.target.coords.y}`;
+  },
+
   // movement control methods
 
   moveForward: function(step = 1) {
     this.coords.y += step;
+    console.log(`${this.name} stepped ${step} forward`);
   },
 
   moveBack: function(step = 1) {
     this.coords.y -= step;
+    console.log(`${this.name} stepped ${step} back`);
   },
 
   moveLeft: function(step = 1) {
     this.coords.x -= step;
+    console.log(`${this.name} stepped ${step} left`);
   },
 
   moveRight: function(step = 1) {
     this.coords.x += step;
+    console.log(`${this.name} stepped ${step} right`);
   },
 }
 
@@ -72,6 +119,11 @@ function Robot(name) {
   };
 };
 
-// creating robot
+// creating default robot
 const lucky = new Robot('lucky');
-console.log(lucky);
+
+lucky.getGreeting();
+
+// type your command below
+
+
